d38065d17ad586b849eab726223f20d03f009bbb
Wen Liang <liangwen12year@gmail.com>
2024-04-22 20:38:13 -0400

    Format python code using black
    Signed-off-by: Wen Liang <liangwen12year@gmail.com>

--- train.py
+++ train.py
@@ -1,53 +1,69 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-from transformers import CodeGenForCausalLM, CodeGenConfig, Trainer, TrainingArguments
-from transformers import GPT2Tokenizer
+# pylint: disable=import-error
+from transformers import (
+    CodeGenForCausalLM,
+    CodeGenConfig,
+    Trainer,
+    TrainingArguments,
+    GPT2Tokenizer,
+)
 from datasets import load_dataset
 from sklearn.model_selection import train_test_split
 import torch
 
+
 def tokenize_function(examples, tokenizer):
     # Adjusted to tokenize the 'content' field
-    tokenized_inputs= tokenizer(examples['content'], truncation=True, padding="max_length", max_length=2048)
-    tokenized_inputs['labels'] = tokenized_inputs['input_ids'].copy()
+    tokenized_inputs = tokenizer(
+        examples["content"], truncation=True, padding="max_length", max_length=2048
+    )
+    tokenized_inputs["labels"] = tokenized_inputs["input_ids"].copy()
     return tokenized_inputs
+
 
 def generate_yaml_files(model, tokenizer, device, num_files=10):
     yaml_files = []
-    prompt = "apiVersion: v1\nkind: "  # Adjust prompt to fit the expected YAML structure
+    prompt = (
+        "apiVersion: v1\nkind: "  # Adjust prompt to fit the expected YAML structure
+    )
 
     for _ in range(num_files):
-        input_ids = tokenizer.encode(prompt, return_tensors='pt').to(device)
+        input_ids = tokenizer.encode(prompt, return_tensors="pt").to(device)
         # Generate sequences
         outputs = model.generate(
             input_ids,
             max_length=2000,  # adjust the max length according to needs
             num_return_sequences=3,
-            temperature=1,  
+            temperature=1,
             no_repeat_ngram_size=2,
-            top_k=50, # select the top k number of next words at each step of the generation
-            top_p=0.9, # select the smallest set of words whose cumulative probability exceeds the threshold p
-            do_sample=True
+            top_k=50,  # select the top k number of next words at each step of the generation
+            top_p=0.9,  # select the smallest set of words whose cumulative probability exceeds the threshold p
+            do_sample=True,
         )
-        
+
         generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
         yaml_files.append(generated_text)
-    
+
     return yaml_files
+
 
 def save_yaml_files(yaml_files):
     for idx, content in enumerate(yaml_files, 1):
-        with open(f"generated_yaml_{idx}.yaml", 'w') as file:
+        with open(f"generated_yaml_{idx}.yaml", "w") as file:
             file.write(content)
+
 
 def main():
     dataset = load_dataset("substratusai/the-stack-yaml-k8s")
     # Tokenize the dataset
     tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     tokenizer.pad_token = tokenizer.eos_token
-    tokenized_datasets = dataset.map(lambda x: tokenize_function(x, tokenizer), batched=True)
-    train_test_split = tokenized_datasets['train'].train_test_split(test_size=0.95)
-    small_train_dataset = train_test_split['train']
+    tokenized_datasets = dataset.map(
+        lambda x: tokenize_function(x, tokenizer), batched=True
+    )
+    train_test_split = tokenized_datasets["train"].train_test_split(test_size=0.95)
+    small_train_dataset = train_test_split["train"]
 
     # Define the model configuration, I get the configuration by printing model config for codegen-350M
     config = CodeGenConfig(
@@ -80,10 +96,8 @@ def main():
         torch_dtype="float16",
         transformers_version="4.33.2",
         use_cache=True,
-        vocab_size=51200
+        vocab_size=51200,
     )
-
-
 
     # Initialize the model from the configuration
     model = CodeGenForCausalLM(config=config)
@@ -97,14 +111,12 @@ def main():
         save_steps=5000,
         save_total_limit=3,  # limits the total amount of checkpoints
         prediction_loss_only=True,
-        gradient_accumulation_steps = 2,
+        gradient_accumulation_steps=2,
         evaluation_strategy="no",
     )
 
     trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=small_train_dataset
+        model=model, args=training_args, train_dataset=small_train_dataset
     )
 
     # Start training
@@ -112,15 +124,13 @@ def main():
     trainer.save_model("./pretrained_yaml_model")
 
     model.eval()
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
     generated_yaml_files = generate_yaml_files(model, tokenizer, device)
     for i, yaml_file in enumerate(generated_yaml_files, 1):
-      print(f"Generated YAML #{i}:\n{yaml_file}\n")
-
+        print(f"Generated YAML #{i}:\n{yaml_file}\n")
 
     save_yaml_files(generated_yaml_files)
-
 
 
 if __name__ == "__main__":
