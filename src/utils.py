e060b100be856ccc247dee2302f920935d68a1f1
Wen Liang <liangwen12year@gmail.com>
2024-05-06 21:48:04 -0400

    Add utility script
    The utility functions will be used in both training script and
    evalution script.
    
    Signed-off-by: Wen Liang <liangwen12year@gmail.com>

---
+++ src/utils.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+"""
+This module provides utility functions for processing data contained in YAML files.
+It includes functions to load YAML data from a file and preprocess it for further
+analysis or processing.
+
+Usage:
+- This module can be imported and its functions invoked in other scripts where YAML
+  data handling is required.
+"""
+
+# pylint: disable=import-error
+import yaml
+
+
+def read_and_preprocess_data(data):
+    processed_data = []
+    for item in data:
+        question = item["question"]
+        answer = item["answer"].strip()  # Ensuring no extra whitespace
+        processed_data.append({"question": question, "answer": answer})
+
+    return processed_data
+
+
+def load_yaml_dataset(file_path):
+    """
+    Load a YAML file and return the data.
+
+    Parameters:
+    file_path (str): The path to the YAML file to be loaded.
+
+    Returns:
+    dict: The data loaded from the YAML file.
+    """
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = yaml.safe_load(file)
+    return data
